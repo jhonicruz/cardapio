6815d908397341b6a22468d77b07b79552804123
Jhoni Cruz <jhonidevpro@gmail.com>
2024-01-02 17:48:40 -0300

    ✨ feat(horario funcionamento):
    add horário de funcionamento

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,9 @@
 import initMenuHamburguer from "./modules/header.js";
 import initgoToStart from "./modules/go-to-start.js";
 import initDepoimentos from "./modules/depoimentos.js";
+import initFuncionamento from "./modules/funcionamento.js";
 
 initMenuHamburguer();
 initgoToStart();
 initDepoimentos();
+initFuncionamento();
