7c1e13373f94131be6b6104ffd29f4aadebfdd2b
Jhoni Cruz <jhonidevpro@gmail.com>
2024-01-02 14:38:09 -0300

    🦄 refactor(depoimento):
    updata code javascript depoimentos section

--- assets/scripts/modules/depoimentos.js
+++ assets/scripts/modules/depoimentos.js
@@ -4,14 +4,17 @@ const depoimentos = document.querySelectorAll("[data-depoimentos='feed']");
 
 const depoimentosBtn = document.querySelectorAll("[data-depoimentos='buttons']");
 
+const events = ["click", "touch"];
+
 depoimentosBtn.forEach((button, index) => {
-  button.addEventListener("click", (e) => {
-    e.preventDefault();
+  events.forEach((userEvent) =>
+    button.addEventListener(userEvent, (e) => {
+      e.preventDefault();
+      depoimentos.forEach((depoimento) => {
+        depoimento.classList.add("hidden");
+      });
 
-    depoimentos.forEach((depoimento) => {
-      depoimento.classList.add("hidden");
-    });
-
-    depoimentos[index].classList.remove("hidden");
-  });
+      depoimentos[index].classList.remove("hidden");
+    })
+  );
 });
