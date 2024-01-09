2debf3a5050243b4ec234a064ec410416b229171
Jhoni Cruz <jhonidevpro@gmail.com>
2024-01-09 14:33:47 -0300

    ✨ feat(cardapio):
    adiciona estrutura básica

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,7 +4,6 @@ import initDepoimentos from "./modules/depoimentos.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initScrollSauve from "./modules/scrollsuave.js";
 import initModalCarrinho from "./modules/modal-carrinho.js";
-import initCardapio from "./modules/cardapio.js";
 
 initMenuHamburguer();
 initgoToStart();
@@ -12,4 +11,3 @@ initDepoimentos();
 initFuncionamento();
 initScrollSauve();
 initModalCarrinho();
-initCardapio();
